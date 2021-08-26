7817f53b9efe71887afc0b0b9baa97344394ee71
Tulip Pandey <65901116+Tulip16@users.noreply.github.com>
2021-08-26 23:16:26 +0530

    updated

--- block.py
+++ block.py
@@ -3,17 +3,17 @@ import numpy as np
 from queue import PriorityQueue
 
 def generate_b_id(p_id, b_id):
-	digits = len(str(b_id))
-	b_num = b_id * (10**digits)
+	digits = len(str(p_id))
+	p_num = p_id * (10**digits)
 
-	return p_id + b_num
+	return p_num + b_id
 
 
 def generate_t_id(p_id, t_id):
-	digits = len(str(t_id))
-	t_num = t_id * (10**digits)
+	digits = len(str(p_id))
+	p_num = p_id * (10**digits)
 
-	return p_id  + t_num
+	return p_num  + t_id
 
 def generate_POW_time(Tk):
 	return math.floor(np.random.exponential(Tk))
@@ -48,6 +48,7 @@ class P2P(object):
 
 	def run(self, time):
 		for i in range(1, time+1):
+			print("running %d"%(i))
 			for peer in self.peers:
 				peer.handle_events(i)
 
@@ -83,9 +84,9 @@ class P2P(object):
 	    	self.current_block = self.p2p.Block(0)
 	    	self.block_chain = []
 	    	self.txn_times = generate_txn_times(10, 5)
-	    	for txn_num in range(len(self.txn_times)):
+	    	for txn_num in range(1, len(self.txn_times)+1):
 	    		t_id = generate_t_id(self.p_id, txn_num)
-	    		self.events.put((self.txn_times[txn_num], self.p2p.Event("gtxn",t_id)))
+	    		self.events.put((self.txn_times[txn_num-1], self.p2p.Event("gtxn",t_id)))
 
 	    def add_event(self, time, event):
 	    	self.events.put((time, event))
@@ -100,13 +101,14 @@ class P2P(object):
 	    	amt = 1
 	    	txn = self.p2p.Transaction(t_id, self.p_id, peer_num, amt)
 	    	self.p2p.transaction_map[txn.t_id] = txn
-	    	print("pid %2d generated a transaction with TxnID: %2d at time: %2d"%(self.p_id, t_id, time))
+	    	print("pid %d generated a transaction with TxnID: %d at time: %d"%(self.p_id, t_id, time))
 	    	for peer in self.p2p.peers:
-	    		event =  self.p2p.Event("rtxn", txn.t_id)
-	    		peer.add_event(time+self.p2p.delays[self.p_id-1][peer.p_id-1], event)
+	    		if(peer.p_id != self.p_id):
+		    		event =  self.p2p.Event("rtxn", txn.t_id)
+		    		peer.add_event(time+self.p2p.delays[self.p_id-1][peer.p_id-1], event)
 
 	    def receive_txn(self, t_id, time):
-	    	print("pid %2d received transaction with TxnID: %2d at time: %2d"%(self.p_id, t_id, time))
+	    	print("pid %d received transaction with TxnID: %d at time: %d"%(self.p_id, t_id, time))
 
 	    def receive_block(self, b_id):
 	    	pass
